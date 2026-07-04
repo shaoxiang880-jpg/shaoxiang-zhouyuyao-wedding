401cc003ae7604285937e700708ba50cddeb1988
shaoxiang880-jpg <shaoxiang880@gmail.com>
2026-07-04 17:22:11 +0800

    Restore H5-style native mini program invite

--- wechat-miniprogram/pages/invite/index.js
+++ wechat-miniprogram/pages/invite/index.js
@@ -2,20 +2,31 @@ const shareTitle = "邵翔 & 周钰瑶 · 婚礼请柬";
 
 Page({
   data: {
-    magazinePages: [
-      "/assets/magazine/page-01.jpg",
-      "/assets/magazine/page-02.jpg",
-      "/assets/magazine/page-03.jpg",
-      "/assets/magazine/page-04.jpg",
-      "/assets/magazine/page-05.jpg",
-      "/assets/magazine/page-06.jpg",
-      "/assets/magazine/page-07.jpg",
-      "/assets/magazine/page-08.jpg",
+    heroPreview: "/assets/h5/couple-main-preview.jpg",
+    heroImage: "/assets/h5/couple-main.jpg",
+    envelopePhoto: "/assets/h5/envelope-photo.jpg",
+    closingPhoto: "/assets/h5/closing-photo.jpg",
+    polaroidPhoto: "/assets/h5/story/story-0-polaroid.jpg",
+    storyPhotos: [
+      "/assets/h5/story/story-1.jpg",
+      "/assets/h5/story/story-2.jpg",
+      "/assets/h5/story/story-3.jpg",
+      "/assets/h5/story/story-4.jpg",
+      "/assets/h5/story/story-5.jpg",
+      "/assets/h5/story/story-6.jpg",
+      "/assets/h5/story/story-7.jpg",
+      "/assets/h5/story/story-8.jpg",
+    ],
+    gridPhotos: [
+      "/assets/h5/story/story-2.jpg",
+      "/assets/h5/story/story-3.jpg",
+      "/assets/h5/story/story-4.jpg",
+      "/assets/h5/story/story-5.jpg",
     ],
     timeline: [
-      { title: "宾客入场", time: "AM 10:30" },
-      { title: "婚礼仪式", time: "AM 11:08" },
-      { title: "婚礼午宴", time: "PM 12:08" },
+      { title: "宾客入场", en: "Guest admission", time: "AM 10:30" },
+      { title: "婚礼仪式", en: "Wedding ceremony", time: "AM 11:08" },
+      { title: "婚礼午宴", en: "Wedding luncheon", time: "PM 12:08" },
     ],
   },
 
